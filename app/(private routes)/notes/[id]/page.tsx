f3b04d7a3bbdf97e1fbe67fb371c15b323041e36
Rostik <rosticmel@gmail.com>
2026-05-17 14:32:44 +0300

    fix - 01

--- app/(private routes)/notes/[id]/page.tsx
+++ app/(private routes)/notes/[id]/page.tsx
@@ -4,7 +4,7 @@ import {
   HydrationBoundary,
   QueryClient,
 } from "@tanstack/react-query";
-import { fetchNoteById } from "@/lib/api/api";
+import { fetchNoteById } from "@/lib/api/serverApi";
 import NoteDetailsClient from "./NoteDetails.client";
 
 interface NoteDetailsPageProps {
